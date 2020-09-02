73e28a323ab5851fdae85e533afecf8b4c2f80c8
Brad <brad.clj@gmail.com>
2020-09-02 11:50:10 -0700

    fib

--- cpp-hr/main.cpp
+++ cpp-hr/main.cpp
@@ -1,3 +1,5 @@
+// https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem
+
 #include <cstddef>
 #include <cstdint>
 
@@ -5,5 +7,25 @@
 using std::cin;
 using std::cout;
 
+int32_t solve(int32_t n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  int32_t a = 0, b = 1, tmp;
+  for (int32_t i=2; i <= n; ++i) {
+    tmp = a + b;
+    a = b;
+    b = tmp;
+  }
+  return b;
+}
+
 int main()
-{}
+{
+  int32_t n;
+  cin >> n;
+  cout << solve(n) << '\n';
+}
