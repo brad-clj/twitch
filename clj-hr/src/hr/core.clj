4eefbf537df290f76cc72317eeb656322f0fb07a
Brad <brad.clj@gmail.com>
2020-09-30 16:25:14 -0700

    maths

--- clj-hr/src/hr/core.clj
+++ clj-hr/src/hr/core.clj
@@ -3,7 +3,12 @@
 (set! *warn-on-reflection* true)
 
 (defn solve
-  [])
+  [n m]
+  (+ (dec n)
+     (* n (dec m))))
 
 (defn main
-  [])
+  []
+  (let [n (read)
+        m (read)]
+    (println (solve n m))))
