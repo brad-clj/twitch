6196faa0c4d5c49a9e3621f91ecda9d2bb893997
Brad <brad.clj@gmail.com>
2020-09-30 16:16:13 -0700

    maths

--- clj-hr/src/hr/core.clj
+++ clj-hr/src/hr/core.clj
@@ -3,7 +3,16 @@
 (set! *warn-on-reflection* true)
 
 (defn solve
-  [])
+  [routes]
+  (reduce (fn [x y]
+            (mod (* x y) 1234567))
+          1 routes))
 
 (defn main
-  [])
+  []
+  (let [t (read)]
+    (-> (repeatedly t (fn []
+                        (let [n (read)
+                              routes (doall (repeatedly (dec n) read))]
+                          (println (solve routes)))))
+        dorun)))
